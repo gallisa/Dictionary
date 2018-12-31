be3b030d5d46499d9543a8c897412b69e9d3f611
Gal Bokobza <gal.bokobza@gmail.com>
2018-12-31 13:55:16 +0200

    d

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApplication6
 {
@@ -12,12 +13,14 @@ namespace ConsoleApplication6
         static void Main(string[] args)
         {
             InitDictionary();
-           
-                ReplaceTextInFile(@"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI1.txt", @"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI2.txt");
-            
+
+            ReplaceTextInFile(@"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI1.txt", @"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI2.txt");
+            ReplacehebTextTogim(@"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI2.txt", @"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI3.txt");
+            //extractheb(@"C:\Users\gb250153\Downloads\WhoAmI\WhoAmI2.txt", @"C:\Users\gb250153\Downloads\WhoAmI\heb.txt");
         }
 
         private static Dictionary<string, string> _myDictionary = new Dictionary<string, string>();
+        private static Dictionary<string, string> _gimDictionary = new Dictionary<string, string>();
 
         private static void InitDictionary()
         {
@@ -49,44 +52,162 @@ namespace ConsoleApplication6
             _myDictionary.Add("u05e9", "ש");
             _myDictionary.Add("u05ea", "ת");
 
+            _gimDictionary.Add("א","1");
+            _gimDictionary.Add("ב","2");
+            _gimDictionary.Add("ג","3");
+            _gimDictionary.Add("ד","4");
+            _gimDictionary.Add("ה","5");
+            _gimDictionary.Add("ו","6");
+            _gimDictionary.Add("ז","7");
+            _gimDictionary.Add("ח","8");
+            _gimDictionary.Add("ט","9");
+            _gimDictionary.Add("י","10");
+            _gimDictionary.Add("ך","20");
+            _gimDictionary.Add("כ","20");
+            _gimDictionary.Add("ל","30");
+            _gimDictionary.Add("ם","40");
+            _gimDictionary.Add("מ","40");
+            _gimDictionary.Add("ן","50");
+            _gimDictionary.Add("נ","50");
+            _gimDictionary.Add("ס","60");
+            _gimDictionary.Add("ע","70");
+            _gimDictionary.Add("ף","80");
+            _gimDictionary.Add("פ","80");
+            _gimDictionary.Add("ץ","90");
+            _gimDictionary.Add("צ","90");
+            _gimDictionary.Add("ק","100");
+            _gimDictionary.Add("ר","200");
+            _gimDictionary.Add("ש","300");
+            _gimDictionary.Add("ת","400");
+
         }
 
         private static void ReplaceTextInFile(string originalFile, string outputFile)
         {
             string tempLineValue;
             //string newLineValue;
-            
-            
-                using (FileStream inputStream = File.OpenRead(originalFile))
-                { 
-                    using (StreamReader inputReader = new StreamReader(inputStream))
-                    {
-                   
-                        using (StreamWriter outputWriter = File.AppendText(outputFile))
-                        {
-                            
-                            while (null != (tempLineValue = inputReader.ReadLine()))
-                            {
-                                foreach (KeyValuePair<string, string> entry in _myDictionary)
-                                {
-                                    if (tempLineValue.Contains(entry.Key))
-                                    {
-                                        string value = entry.Value;
-                                        tempLineValue = tempLineValue.Replace(entry.Key, value);
-                                    }
 
-                                
-                                       
+
+            using (FileStream inputStream = File.OpenRead(originalFile))
+            {
+                using (StreamReader inputReader = new StreamReader(inputStream))
+                {
+
+                    using (StreamWriter outputWriter = File.AppendText(outputFile))
+                    {
+
+                        while (null != (tempLineValue = inputReader.ReadLine()))
+                        {
+                            foreach (KeyValuePair<string, string> entry in _myDictionary)
+                            {
+                                if (tempLineValue.Contains(entry.Key))
+                                {
+                                    string value = entry.Value;
+                                    tempLineValue = tempLineValue.Replace(entry.Key, value);
                                 }
+
+
+
+                            }
                             outputWriter.WriteLine(tempLineValue);
                             Console.WriteLine(tempLineValue);
                             Console.ReadLine();
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private static void ReplacehebTextTogim(string textFile, string gimFile)
+        {
+            string tempLineValue2;
+            //string newLineValue;
+
+
+            using (FileStream inputStream2 = File.OpenRead(textFile))
+            {
+                using (StreamReader inputReader2 = new StreamReader(inputStream2))
+                {
+
+                    using (StreamWriter outputWriter2 = File.AppendText(gimFile))
+                    {
+
+                        while (null != (tempLineValue2 = inputReader2.ReadLine()))
+                        {
+                            foreach (KeyValuePair<string, string> entry in _gimDictionary)
+                            {
+                                if (tempLineValue2.Contains(entry.Key))
+                                {
+                                    string value = entry.Value;
+                                    tempLineValue2 = tempLineValue2.Replace(entry.Key, value);
+                                }
+
+
+
                             }
-                        
+                            outputWriter2.WriteLine(tempLineValue2);
+                            Console.WriteLine(tempLineValue2);
+                            Console.ReadLine();
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+
+        private static void extractheb(string originalengFile, string outputhebFile)
+        {
+            string ch;
+            string tempLine;
+           
+            
+
+            using (FileStream inputStream1 = File.OpenRead(originalengFile))
+            {
+                using (StreamReader inputReader1 = new StreamReader(inputStream1))
+                {
+
+                    using (StreamWriter outputWriter1 = File.AppendText(outputhebFile))
+                    {
+
+                        while (null != (tempLine = inputReader1.ReadLine()))
+                        {
+                            ch = string.Empty;
+                            foreach (char c in tempLine)
+                            {
+
+                                //ch += c;
+                                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c>= '0' && c<='9')
+                                {
+                                   // Console.WriteLine(c);
+                                }
+                                else
+                                {
+                                    ch += c;
+                                    //Console.WriteLine(ch);
+                                }
+                            }
+                            
+                            Console.WriteLine(ch);
+                            outputWriter1.WriteLine(ch);
+
+                            //if (reg.Match(ch).Success)
+                            //{
+                            //    Console.WriteLine(ch);
+                            //}
+                            //Console.WriteLine(tempLine);
+                            //Console.WriteLine(ch);
+                            Console.ReadLine();
                         }
                     }
                 }
+            }
         }
+
+
     }    
 }
 
